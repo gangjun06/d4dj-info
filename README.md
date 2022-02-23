4d3d8dedc490f3ac39615bdec899e30f97ed6e65
Gangjun Lee <me@gangjun.dev>
2022-02-23 22:40:09 +0900

    add umami analytics

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # D4DJ.info
+
+D4DJ Project's Information Website
+
+## Usage
+
+```
+set [.env.local]:
+NEXT_PUBLIC_BACKEND_URL=(Backend URL)
+//NEXT_PUBLIC_UMAMI_WEB_ID=(Umami Analytics id)
+//NEXT_PUBLIC_UMAMI_SRC=(Umami Analytics src)
+
+start development server:
+yarn install
+yarn dev
+```
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,6 +3,7 @@ import { ApolloProvider } from '@apollo/client'
 import { client } from 'lib/apollo'
 import setLanguage from 'next-translate/setLanguage'
 import type { AppProps } from 'next/app'
+import Head from 'next/head'
 import { useRouter } from 'next/router'
 import NextNProgress from 'nextjs-progressbar'
 import { useEffect } from 'react'
@@ -20,11 +21,23 @@ function MyApp({ Component, pageProps }: AppProps) {
   }, [])
 
   return (
-    <ApolloProvider client={client}>
-      <NextNProgress color={'#6366f1'} />
-      <ToastContainer position="top-center" />
-      <Component {...pageProps} />
-    </ApolloProvider>
+    <>
+      <Head>
+        <Head>
+          <script
+            async
+            defer
+            data-website-id={process.env.NEXT_PUBLIC_UMAMI_WEB_ID}
+            src={process.env.NEXT_PUBLIC_UMAMI_SRC}
+          ></script>
+        </Head>
+      </Head>
+      <ApolloProvider client={client}>
+        <NextNProgress color={'#6366f1'} />
+        <ToastContainer position="top-center" />
+        <Component {...pageProps} />
+      </ApolloProvider>
+    </>
   )
 }
 export default MyApp
